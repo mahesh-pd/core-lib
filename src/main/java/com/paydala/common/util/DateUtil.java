07aa9918504f2f6810e0de529e75ad3f99f1c8cb
Mahesh Viraktamath <mahesh@sharevault.com>
2022-01-31 21:47:13 +0530

    added date util for ISO utc timestamp

---
+++ src/main/java/com/paydala/common/util/DateUtil.java
@@ -0,0 +1,31 @@
+package com.paydala.common.util;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class DateUtil {
+
+    public static String toISO8601UTC(Date date) {
+        TimeZone tz = TimeZone.getTimeZone("UTC");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        df.setTimeZone(tz);
+        return df.format(date);
+    }
+
+    public static Date fromISO8601UTC(String dateStr) {
+        TimeZone tz = TimeZone.getTimeZone("UTC");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        df.setTimeZone(tz);
+
+        try {
+            return df.parse(dateStr);
+        } catch (ParseException e) {
+
+        }
+
+        return null;
+    }
+}
