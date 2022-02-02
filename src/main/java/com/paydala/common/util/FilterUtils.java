32c2012e6d5febac6ec7abca05ee836ce47b0e28
Mahesh Viraktamath <mahesh@sharevault.com>
2022-02-02 19:41:43 +0530

    added common filter util to add CORS headers for dev env

--- src/main/java/com/paydala/common/util/FilterUtils.java
+++ src/main/java/com/paydala/common/util/FilterUtils.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.function.Function;
@@ -24,5 +25,26 @@ public class FilterUtils {
         }
 
         return true;
+    }
+
+    public static void addCORSHeaders(HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+        response.addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Disposition, OrganizationId, ConversationalCloudAuthorization");
+        //https://globaljira.liveperson.com/browse/LE-94678
+
+        //https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+        response.addHeader("x-content-type-options", "nosniff");
+        //https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+        response.addHeader("strict-transport-security", "max-age=31536000");
+        //https://content-security-policy.com/
+        response.addHeader("content-security-policy",
+                "default-src *.liveperson.net *.botcentralai.com;"
+                        + " script-src *.liveperson.net *.botcentralai.com;"
+                        + " connect-src *.liveperson.net *.botcentralai.com *.botcentralapi.com;"
+//                          + " img-src *.liveperson.net *.botcentralai.com;"
+                        + " style-src *.liveperson.net *.botcentralai.com;");
+        //https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection
+        response.addHeader("X-XSS-Protection", "1");
     }
 }
