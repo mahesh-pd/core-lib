accd9574d9a786acfed91a054433f0e8e4b2226d
ZEUS\Susheela <susheela@paydala.com>
2022-02-03 17:14:51 +0530

    Changes related to KYC metrics for parnter dashboard

--- src/main/java/com/paydala/common/data/auth/access/KycRepository.java
+++ src/main/java/com/paydala/common/data/auth/access/KycRepository.java
@@ -6,9 +6,12 @@
 package com.paydala.common.data.auth.access;
 
 import com.paydala.common.data.auth.entity.Kyc;
+import java.util.Date;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 /**
@@ -16,5 +19,12 @@ import org.springframework.data.repository.query.Param;
  * @author Susheela
  */
 public interface KycRepository  extends JpaRepository<Kyc, Integer>{
-    List<Kyc> findByUserId(@Param("user") Integer id );
+    List<Kyc> findByUserId(@Param("user") Integer id ); 
+    
+    List<Kyc> findByLastKycBetween(@Param("lastkyc") Date fromDate, @Param("laskyyc") Date toDate);
+    
+    @Modifying
+    @Query("select k from Kyc k where k.lastKyc between :fromDate and :toDate and k.status=:status")
+    List<Kyc> findByLastKycBetweenAndStatus(@Param("fromDate") Date fromDate, @Param("toDate") Date toDate, @Param("status") Boolean status);
+    
 }
--- src/main/java/com/paydala/common/util/DateUtil.java
+++ src/main/java/com/paydala/common/util/DateUtil.java
@@ -3,6 +3,8 @@ package com.paydala.common.util;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.TimeZone;
 import org.slf4j.Logger;
@@ -49,6 +51,27 @@ public class DateUtil {
             LOG.error("Error parsing the date ", ex);
             return null;
         }
+    }
+     
+     public static Instant getDateFromDateFilter(String dateFilter,Instant toDate){
+        return  toDate.minus(getHoursFromDateFilter(dateFilter), ChronoUnit.HOURS);
+    }
 
+    private static int getHoursFromDateFilter(String dateFilter ){
+        int number=getNumberFromDateFilter(dateFilter);
+        if(dateFilter.contains("day"))
+            return getHoursFromDay(number);
+        else if(dateFilter.contains("hr"))
+            return number;
+        //TODO: Better handling for all. Rn only for 1 year
+        return 8760;
+    }
+
+    private static int getHoursFromDay(int day ){
+        return day*24;
+    }
+
+    private static int getNumberFromDateFilter(String dateFilter){
+        return Integer.parseInt(dateFilter.replaceAll("[^0-9]", ""));
     }
 }
